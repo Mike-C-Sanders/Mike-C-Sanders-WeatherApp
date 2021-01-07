52c8b1ddaa490d4f038fdcb64392dcce54a2e994
Mike Sanders <71601403+Mike-C-Sanders@users.noreply.github.com>
2021-01-07 10:30:11 -0800

    Updated app.js
    Added the base URL from openweathermap.com
    
    added the api key. 
    
    Created perform action function to kick off the api calls.

--- website/app.js
+++ website/app.js
@@ -1,5 +1,16 @@
 /* Global Variables */
 
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+let apiKey = '7ddac6b965b24370c39787b9c1d7fc4d';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//event listener to determine if an action has been performed.
+document.getElementById('generate').addEventListener('click', performAction);
+
+//action performed when clicking the generate button.
+function performAction(e){
+    
+}
+
