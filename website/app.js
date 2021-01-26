fafd95cea475a968b1ffea63e99cdc305990146b
Mike Sanders <71601403+Mike-C-Sanders@users.noreply.github.com>
2021-01-26 13:08:25 -0800

    Updated app.js
    Removed console.log statements tracking errors. 
    
    Updated the month to display the date correctly. 
    
    Updated the url and api variables from let to const.

--- website/app.js
+++ website/app.js
@@ -1,14 +1,14 @@
 
 //base url to openweathermap.org/
-let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
-let apiKey = '&appid=7ddac6b965b24370c39787b9c1d7fc4d';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '&appid=7ddac6b965b24370c39787b9c1d7fc4d';
 //since we're using zipcode we need to have the country code as part of the called variable
 let countryCode = ',us';
 //units for Fahrenheit Units using units=imperial
 let tempUnits = '&units=imperial'
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 //event listener to determine if an action has been performed.
 document.getElementById('generate').addEventListener('click', doPromisedAction);
@@ -26,8 +26,7 @@ const performAction = async (e) =>{
         const data = await getWeatherData(baseURL, newZip, apiKey, countryCode, tempUnits);
          
             //Add data to the server object before posting to UI
-            console.log('2 then add to server');
-        await postData('/addWeather', {temperature: data.main.temp,
+        await postData('/addWeather', {temperature: data.main.temp, 
                 location: data.name, date: newDate, content: feelings});
     
         await updateUI();
@@ -42,7 +41,6 @@ const getWeatherData = async (bURL, zip, key, code, units)=>{
     const res = await fetch(bURL+zip+code+units+key);
     try{
         const data = await res.json();
-        console.log('1 Get data');
         return data;
     }catch(error){
         console.log('error', error);
@@ -51,7 +49,6 @@ const getWeatherData = async (bURL, zip, key, code, units)=>{
 
 //Post data function used for the weather posting to server.
 const postData = async (url = '', data = {}) =>{
-    console.log('3 data to server');
     const response = await fetch(url, {
         method: 'POST', //Post route method
         credentials: 'same-origin',
@@ -62,7 +59,6 @@ const postData = async (url = '', data = {}) =>{
     });
     try{
         const newData = await response.json();
-        console.log('4 Posting newData');
         return newData;
     }catch(error){
         console.log('error', error);
@@ -71,13 +67,9 @@ const postData = async (url = '', data = {}) =>{
 
 const updateUI = async()=>{
     //fetching from the local server
-    console.log('before request');
     const request = await fetch('/all');
     try{
-        console.log('after request');
-        console.log(request);
         const allData = await request.json();
-        console.log(allData[0].icon);
         document.getElementById('content').innerHTML = 'You feel: '+ allData[0].content;
         document.getElementById('date').innerHTML = `Today's Date: ` + allData[0].date;
         document.getElementById('temp').innerHTML = 'Temprature: ' + allData[0].temperature + 'F';
